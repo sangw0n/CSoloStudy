21fcc6d1328faeb69fd4f526b65e2c51ab9f5ec2
sangw0n <pilwon070127@naver.com>
2024-04-27 18:15:53 +0900

    [Add] C Study Code File

--- CVariable.c
+++
@@ -1,9 +0,0 @@
-
-#include<stdio.h>
-
-int main(void)
-{
-	// Study Start
-
-	return 0;
-}
---
+++ HelloWorld.c
@@ -0,0 +1,7 @@
+// 전처리기 : '사전, 미리'에 처리한다는 뜻 
+//#include <stdio.h>
+//
+//void main()
+//{
+//	printf("Hello, World!");
+//}
