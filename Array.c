1a8c84bf513664878c2f166c75d4ae587913a310
sangw0n <pilwon070127@naver.com>
2024-05-06 23:20:12 +0900

    [Add] C Study Code File

---
+++ Array.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	//char result[255];
+	//char data[255] = "Hello";
+
+	//strcpy(result, data);
+	//strcat(result, " world!");
+
+	//printf("%s", result);
+
+	//char temp[2][4] = { {1,2,3}, {4,5,6} }; // 2За 4ї­
+	//for (int i = 0; i < 2; i++)
+	//{
+	//	for (int j = 0; j < 3; j++)
+	//	{
+	//		printf("%d ", temp[i][j]);
+	//	}
+	//	printf("\n");
+	//}
+
+	//short data[9] = { 4, 6, 9, 8, 7, 2, 5, 1, 3 };
+	//short result = 0;
+
+	//for (int i = 0; i < 9; i += 2)
+	//{
+	//	result += data[i];
+	//	printf("%d\n", data[i]);
+	//}
+	//printf("%d", result);
+
+	//short data[9] = { 4, 6, 9, 8, 7, 2, 5, 1, 3 };
+	//short temp;
+
+	//for (short i = 0; i < 9; i++)
+	//{
+	//	if (temp < data[i])
+	//		temp = data[i];
+	//}
+
+	//printf("%d", temp);
+
+	//char data[3][4] = { {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0} };
+	//data[1][1] = 1;
+	//data[2][3] = 2;
+
+
+}
